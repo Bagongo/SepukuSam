49df654f8581399497e0514fdb9446d035572f18
Bagongo <akak@hotmail.it>
2016-07-29 23:08:18 +0200

    added animations, sprites, part system

---
+++ Assets/Scripts/CameraSettings.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraSettings : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+		print(Screen.width + " " + Screen.height);
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/ScoreMan.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+
+public class ScoreMan : MonoBehaviour {
+
+	public Text scoreDisplayer;
+	public int scoreCount;
+
+	private string fixedTxt;
+
+	void Start () { 
+
+		fixedTxt = scoreDisplayer.text;
+		scoreCount = 0;
+
+		UpdateScore(scoreCount);
+	
+	}
+	
+	void Update () {
+	
+	}
+
+	public void UpdateScore(int score)
+	{
+		scoreCount += score;
+		scoreDisplayer.text = fixedTxt + " " + scoreCount;
+	}
+}
